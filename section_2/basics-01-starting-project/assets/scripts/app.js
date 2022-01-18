42f24f1c4d4352394b18be453b3beea70082a4fa
Jerika Kanz <jerikacapila@gmail.com>
2022-01-18 05:40:38 +0100

    Splitting Code into Functions

--- section_2/basics-01-starting-project/assets/scripts/app.js
+++ section_2/basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  currentResult += parseInt(userInput.value); //Can also use the plus(+) sign in FRONT of the string to convert strings to numbers
-  outputResult(currentResult, "");
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult += enteredNumber; //Can also use the plus(+) sign in FRONT of the string to convert strings to numbers
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener("click", add);
