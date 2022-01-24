c43e481e7ada7fc6c5542dd1b77460ed249105ae
Jerika Kanz <jerikacapila@gmail.com>
2022-01-24 10:55:29 +0100

    Inside the JavaScript Engine - How the Code Executes

--- section_5/behind-scenes-01-starting-setup/app.js
+++ section_5/behind-scenes-01-starting-setup/app.js
@@ -1,23 +1,10 @@
-// let name = "Max";
-// let hobbies;
+function getName() {
+  return prompt("Your name: ", "");
+}
 
-// if (name === "Max") {
-//   let hobbies = ["Sports", "Cooking"];
-//   console.log(hobbies);
-// }
+function greet() {
+  const userName = getName();
+  console.log("Hello " + userName);
+}
 
-// function greet() {
-//   let age = 30;
-//   let name = "Manuel";
-//   console.log(name, age, hobbies);
-// }
-
-// console.log(name, hobbies);
-
-// greet();
-"use strict";
-
-const userName = "Max";
-var undefined = 5;
-
-console.log(userName);
+greet();
