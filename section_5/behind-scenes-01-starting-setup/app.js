f5cb0b42875c51cbe81d52e10fc71a31a2d28ffd
Jerika Kanz <jerikacapila@gmail.com>
2022-01-24 09:50:34 +0100

    Understanding "Hoisting"

--- section_5/behind-scenes-01-starting-setup/app.js
+++ section_5/behind-scenes-01-starting-setup/app.js
@@ -1,17 +1,21 @@
-var name = "Max";
-let hobbies;
+// let name = "Max";
+// let hobbies;
 
-if (name === "Max") {
-  let hobbies = ["Sports", "Cooking"];
-  console.log(hobbies);
-}
+// if (name === "Max") {
+//   let hobbies = ["Sports", "Cooking"];
+//   console.log(hobbies);
+// }
 
-function greet() {
-  let age = 30;
-  let name = "Manuel";
-  console.log(name, age, hobbies);
-}
+// function greet() {
+//   let age = 30;
+//   let name = "Manuel";
+//   console.log(name, age, hobbies);
+// }
 
-console.log(name, hobbies);
+// console.log(name, hobbies);
 
-greet();
+// greet();
+
+console.log(userName);
+
+var userName = "Max";
