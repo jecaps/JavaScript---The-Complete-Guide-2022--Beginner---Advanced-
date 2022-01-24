fe2a3372d10ff83d4253ed30c069090571da45e7
Jerika Kanz <jerikacapila@gmail.com>
2022-01-24 08:56:16 +0100

    var vs let and const - Introducing "Block Scope"

---
+++ section_5/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,17 @@
+var name = "Max";
+let hobbies;
+
+if (name === "Max") {
+  let hobbies = ["Sports", "Cooking"];
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 30;
+  let name = "Manuel";
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();
