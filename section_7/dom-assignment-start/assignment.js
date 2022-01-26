d191658e283fe78cea5d82ce6501d3d69d499deb
Jerika Kanz <jerikacapila@gmail.com>
2022-01-26 20:14:01 +0100

    Assignment 5: Time to Practice - DOM Querying

---
+++ section_7/dom-assignment-start/assignment.js
@@ -0,0 +1,13 @@
+const firstListItem = document.getElementById("task-1");
+const firstListItem1 = document.querySelector("#task-1");
+
+firstListItem.style.color = "white";
+firstListItem.style.backgroundColor = "black";
+
+const title = document.querySelector("title");
+const title2 = document.head.querySelector("title");
+title.textContent = "Assignment - Solved!";
+title2.textContent = "Assignment - Solved!";
+
+const h1 = document.querySelector("h1");
+h1.textContent = "Assignment - Solved!";
