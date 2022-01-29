9545c79723dbb4dd0d6e8731f524a12e0e59cdaf
Jerika Kanz <jerikacapila@gmail.com>
2022-01-29 20:14:18 +0100

    Finding Stuff: find() and findIndex()

--- section_8/arrays-01-starting-code/app.js
+++ section_8/arrays-01-starting-code/app.js
@@ -57,3 +57,16 @@ console.log(testResults.lastIndexOf(1.5));
 
 const personData = [{ name: "Max" }, { name: "Manuel" }];
 console.log(personData.indexOf({ name: "Manuel" }));
+
+const manuel = personData.find((person, idx, persons) => {
+  return person.name === "Manuel";
+});
+
+manuel.name = "Anna";
+console.log(manuel, personData);
+
+const maxIndex = personData.findIndex((person, idx, persons) => {
+  return person.name === "Max";
+});
+
+console.log(maxIndex);
