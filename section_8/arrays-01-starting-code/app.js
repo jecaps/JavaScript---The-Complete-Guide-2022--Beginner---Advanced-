0ce646a4ecba021bee9941eb6c215359977ad180
Jerika Kanz <jerikacapila@gmail.com>
2022-01-30 14:07:26 +0100

    Transforming Data with map()

--- section_8/arrays-01-starting-code/app.js
+++ section_8/arrays-01-starting-code/app.js
@@ -74,13 +74,23 @@
 
 // console.log(maxIndex);
 
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
+
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices = [];
 
-prices.forEach((price, idx, prices) => {
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
