e18d6d56892847d61bdb80f52f0b7cf5c5854198
Jerika Kanz <jerikacapila@gmail.com>
2022-01-31 16:00:04 +0100

    Dynamic Property Access and Setting Properties Dynamically

--- section_9/objects-01-starting-project/assets/scripts/objects.js
+++ section_9/objects-01-starting-project/assets/scripts/objects.js
@@ -3,10 +3,13 @@
 // movieList.style["background-color"] = "red";
 // movieList.style.display = "block";
 
+const useChosenKeyName = "level";
+
 let person = {
   "first name": "Max",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  [userChosenKeyName]: "...",
   greet: function () {
     alert("Hi there!");
   },
@@ -19,5 +22,8 @@ delete person.age;
 // person.age = null;
 person.isAdmin = true;
 
+const keyName = "first name";
+
+console.log(person[keyName]);
 console.log(person["hobbies"]);
 console.log(person[1.5]);
