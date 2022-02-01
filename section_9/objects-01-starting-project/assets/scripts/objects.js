6a06774c1f70c8ef39519af22adf3a5e2ad529bc
Jerika Kanz <jerikacapila@gmail.com>
2022-02-01 12:05:43 +0100

    The 'this' Keyword and Its Strange Behavior

--- section_9/objects-01-starting-project/assets/scripts/objects.js
+++ section_9/objects-01-starting-project/assets/scripts/objects.js
@@ -23,9 +23,9 @@ const renderMovies = (filter = "") => {
     const { info, ...otherProps } = movie;
     console.log(otherProps);
     // const { title: movieTitle } = info;
-    // const {getFormattedTitle} = movie;
-
-    let text = movie.getFormattedTitle() + " - ";
+    let { getFormattedTitle } = movie;
+    getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle() + " - ";
     for (const key in info) {
       if (key !== "title") {
         text = text + `${key}: ${info[key]}`;
@@ -56,6 +56,7 @@ const addMovieHandler = () => {
     },
     id: Math.random().toString(),
     getFormattedTitle() {
+      console.log(this);
       return this.info.title.toUpperCase();
     },
   };
