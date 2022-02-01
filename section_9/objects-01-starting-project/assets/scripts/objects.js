51e8608bfbc372e5b18b5e994a0010aa05223497
Jerika Kanz <jerikacapila@gmail.com>
2022-02-01 11:26:30 +0100

    The Method Shorthand Syntax

--- section_9/objects-01-starting-project/assets/scripts/objects.js
+++ section_9/objects-01-starting-project/assets/scripts/objects.js
@@ -55,7 +55,7 @@ const addMovieHandler = () => {
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
-    getFormattedTitle: function () {
+    getFormattedTitle() {
       return this.info.title.toUpperCase();
     },
   };
