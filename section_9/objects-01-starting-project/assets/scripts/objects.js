aa21b6f5f4d88ae41ab8e000f99b64d4c93a3283
Jerika Kanz <jerikacapila@gmail.com>
2022-02-01 19:45:23 +0100

    Getters and Setters

--- section_9/objects-01-starting-project/assets/scripts/objects.js
+++ section_9/objects-01-starting-project/assets/scripts/objects.js
@@ -27,7 +27,7 @@ const renderMovies = (filter = "") => {
     // getFormattedTitle = getFormattedTitle.bind(movie);
     let text = getFormattedTitle.call(movie) + " - ";
     for (const key in info) {
-      if (key !== "title") {
+      if (key !== "title" && key !== "_title") {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -41,17 +41,22 @@ const addMovieHandler = () => {
   const extraName = document.getElementById("extra-name").value;
   const extraValue = document.getElementById("extra-value").value;
 
-  if (
-    title.trim() === "" ||
-    extraName.trim() === "" ||
-    extraValue.trim() === ""
-  ) {
+  if (extraName.trim() === "" || extraValue.trim() === "") {
     return;
   }
 
   const newMovie = {
     info: {
-      title,
+      set title(val) {
+        if (val.trim() === "") {
+          this._title = "DEFAULT";
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
@@ -60,6 +65,9 @@ const addMovieHandler = () => {
       return this.info.title.toUpperCase();
     },
   };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
 
   movies.push(newMovie);
   renderMovies();
