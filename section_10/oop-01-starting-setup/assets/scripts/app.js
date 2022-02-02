e923dbd94e8442eee44c71a968fa1d04084de13a
Jerika Kanz <jerikacapila@gmail.com>
2022-02-02 09:22:45 +0100

    Working with Constructor Methods

--- section_10/oop-01-starting-setup/assets/scripts/app.js
+++ section_10/oop-01-starting-setup/assets/scripts/app.js
@@ -3,27 +3,29 @@ class Product {
   imageUrl;
   description;
   price;
-}
 
-console.log(new Product());
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
 
 const productList = {
   products: [
-    // new Product()
-    {
-      title: "A Pillow",
-      imageUrl:
-        "https://images.pexels.com/photos/776120/pexels-photo-776120.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      price: 19.99,
-      description: "A soft pillow!",
-    },
-    {
-      title: "A Carpet",
-      imageUrl:
-        "https://images.pexels.com/photos/276583/pexels-photo-276583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      price: 89.99,
-      description: "A carpet which you might like -  or not.",
-    },
+    new Product(
+      "A Pillow",
+      "https://images.pexels.com/photos/776120/pexels-photo-776120.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      "A soft pillow!",
+      19.99
+    ),
+    new Product(
+      "A Carpet",
+      "https://images.pexels.com/photos/276583/pexels-photo-276583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      "A carpet which you might like -  or not.",
+      89.99
+    ),
   ],
   render() {
     const rederHook = document.getElementById("app");
