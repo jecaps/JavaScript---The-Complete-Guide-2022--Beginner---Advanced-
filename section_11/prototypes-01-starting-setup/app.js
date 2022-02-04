bd596e20af51f41b07b65d8095e7d7e8a4067930
Jerika Kanz <jerikacapila@gmail.com>
2022-02-04 17:12:54 +0100

    Setting and Getting Prototypes

--- section_11/prototypes-01-starting-setup/app.js
+++ section_11/prototypes-01-starting-setup/app.js
@@ -10,40 +10,45 @@ class Person {
   constructor() {
     // super();
     this.age = 30;
-    // this.greet = function() {...}
+    // this.greet = function() { ... }
   }
 
-  greet = () => {
+  // greet = () => {
+  //   console.log(
+  //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+  //   );
+  // };
+
+  greet() {
     console.log(
       "Hi, I am " + this.name + " and I am " + this.age + " years old."
     );
-  };
-
-  // greet() {
-  //   console.log(
-  //     "Hi, I am " + this.name + " and I am " + this.age + " years old."
-  //   );
-  // }
+  }
 }
 
 // function Person() {
 //   this.age = 30;
-//   this.name = "Max";
-//   // this.greet = function() {...}
-
+//   this.name = 'Max';
+//   // this.greet = function() { ... };
 // }
 
-// Person.prototype.greet() = function () {
-//   console.log("Hi, I am " + this.name + " and I am " + this.age + " years old.")
+// Person.prototype.greet = function() {
+//   console.log(
+//     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//   );
+// };
+
+// Person.describe = function() {
+//   console.log('Creating persons...');
 // }
 
 // Person.prototype = {
 //   printAge() {
 //     console.log(this.age);
-//   },
+//   }
 // };
 
-// Person.prototype.printAge = function () {
+// Person.prototype.printAge = function() {
 //   console.log(this.age);
 // };
 
@@ -52,15 +57,60 @@ class Person {
 // const p = new Person();
 // p.greet();
 // p.printAge();
-// console.log(p);
+// console.log(p.length);
 // console.log(p.toString());
 // const p2 = new p.__proto__.constructor();
-// console.dir(Object);
+// console.dir(Object.prototype.__proto__);
 
-const p = new Person();
-const p2 = new Person();
-p.greet();
-console.log(p);
+// const p = new Person();
+// const p2 = new Person();
+// p.greet();
+// console.log(p);
 
-const button = document.getElementById("btn");
-button.addEventListener("click", p.greet);
+// const button = document.getElementById('btn');
+// button.addEventListener('click', p.greet.bind(p));
+
+const course = {
+  // new Object()
+  title: "JavaScript - The Complete Guide",
+  rating: 5,
+};
+
+// console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating: function () {
+    console.log(`${this.rating}/5`);
+  },
+});
+
+const student = Object.create(
+  {
+    printProgress: function () {
+      console.log(this.progress);
+    },
+  },
+  {
+    name: {
+      configurable: true,
+      enumerable: true,
+      value: "Max",
+      writable: true,
+    },
+  }
+);
+
+// student.name = 'Max';
+
+Object.defineProperty(student, "progress", {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false,
+});
+
+student.printProgress();
+
+console.log(student);
+
+course.printRating();
