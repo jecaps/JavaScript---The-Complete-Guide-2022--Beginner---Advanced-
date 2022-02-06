6614a3240f4d5186482a6bd9efb71913f25ce8c9
Jerika Kanz <jerikacapila@gmail.com>
2022-02-06 11:30:57 +0100

    Starting with the 'Switch Project' Logic

--- section_12/practice-oop-01-starting-setup/assets/scripts/app.js
+++ section_12/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,11 +1,38 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  construtor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton() {}
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switcBtn.addEventListener("click");
+  }
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.project.splice(projectId, 1);
+    this.addProject();
+    this.projects = this.projects.filter((p) => p.id === projectId);
   }
 }
 
