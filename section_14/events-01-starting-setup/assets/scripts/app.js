0a090e00446e03dcd88f2d35071e1802a8f11ea9
Jerika Kanz <jerikacapila@gmail.com>
2022-02-08 18:56:40 +0100

    Marking the 'Drop Area'

--- section_14/events-01-starting-setup/assets/scripts/app.js
+++ section_14/events-01-starting-setup/assets/scripts/app.js
@@ -148,6 +148,30 @@ class ProjectList {
       );
     }
     console.log(this.projects);
+    this.connectDroppable();
+  }
+
+  connectDroppable() {
+    const list = document.querySelector(`#${this.type}-projects ul`);
+
+    list.addEventListener("dragenter", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+      list.parentElement.classList.add("droppable");
+    });
+
+    list.addEventListener("dragover", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener("dragleave", (event) => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove("droppable");
+      }
+    });
   }
 
   setSwitchHandlerFunction(switchHandlerFunction) {
