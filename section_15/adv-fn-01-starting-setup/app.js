38b654782f08a9ac734e1afc2b182e1cc325f91d
Jerika Kanz <jerikacapila@gmail.com>
2022-02-09 10:12:44 +0100

    Pure Functions and Side Effects

---
+++ section_15/adv-fn-01-starting-setup/app.js
@@ -0,0 +1,36 @@
+// Pure Function
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(1, 5)); // 6
+console.log(add(12, 15)); // 27
+
+// Impure Function
+function addRandom(num1) {
+  return num1 + Math.random();
+}
+
+console.log(addRandom(5));
+
+let previousResult = 0;
+
+function addMoreNumbers(num1, num2) {
+  const sum = num1 + num2;
+  previousResult = sum;
+  return sum;
+}
+
+console.log(addMoreNumbers(1, 5));
+console.log(previousResult);
+
+const hobbies = ["Sports", "Cooking"];
+
+function printHobbies(h) {
+  h.push("NEW HOBBY");
+  console.log(h);
+}
+
+printHobbies(hobbies);
+
+console.log(hobbies);
