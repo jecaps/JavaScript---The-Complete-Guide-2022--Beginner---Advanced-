af4126bb133011d4a5a61b36f08c65d10c0d8f96
Jerika Kanz <jerikacapila@gmail.com>
2022-02-09 20:10:01 +0100

    Advanced Recursion

--- section_15/adv-fn-01-starting-setup/app.js
+++ section_15/adv-fn-01-starting-setup/app.js
@@ -68,3 +68,68 @@ let name = "Maximilian";
 userName = "Manuel";
 
 greetUser();
+
+{
+  const age = 30;
+  console.log(age); // 30
+}
+
+// function powerOf(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+//   return result;
+// }
+
+function powerOf(x, n) {
+  // if (n === 1) {
+  //   return x;
+  // }
+  // return x * powerOf(x, n - 1);
+  return n === 1 ? x : x * powerOf(x, n - 1);
+}
+console.log(powerOf(2, 3)); // 2*2*2
+
+const myself = {
+  name: "Max",
+  friends: [
+    {
+      name: "Manuel",
+      friends: [
+        {
+          name: "Chris",
+          friends: [
+            {
+              name: "Hari",
+            },
+            {
+              name: "Amilia",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Julia",
+    },
+  ],
+};
+
+function getFriendNames(person) {
+  const collectedNames = [];
+
+  if (!person.friends) {
+    return [];
+  }
+
+  for (const friend of person.friends) {
+    collectedNames.push(friend.name);
+    collectedNames.push(...getFriendNames(friend));
+  }
+
+  return collectedNames;
+}
+
+console.log(getFriendNames(myself));
