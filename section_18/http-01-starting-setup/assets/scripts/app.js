9207a7b86377dafd2488f1f19b3156e973f91897
Jerika Kanz <jerikacapila@gmail.com>
2022-02-23 11:52:31 +0100

    JSON Data and Parsing Data

--- section_18/http-01-starting-setup/assets/scripts/app.js
+++ section_18/http-01-starting-setup/assets/scripts/app.js
@@ -1,5 +1,21 @@
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+
 const xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+
+xhr.responseType = "json";
+
+xhr.onload = function () {
+  // const listsOfPosts = JSON.parse(xhr.response);
+  const listsOfPosts = xhr.response;
+  for (const post of listsOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
+};
 
 xhr.send();
