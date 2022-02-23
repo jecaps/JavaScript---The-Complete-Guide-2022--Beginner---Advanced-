bb7f6b935973d7d235c2faef160b857b8b74873b
Jerika Kanz <jerikacapila@gmail.com>
2022-02-23 20:21:45 +0100

    Handling Errors

--- section_18/http-01-starting-setup/assets/scripts/app.js
+++ section_18/http-01-starting-setup/assets/scripts/app.js
@@ -13,8 +13,17 @@ function sendHttpRequest(method, url, data) {
     xhr.responseType = "json";
 
     xhr.onload = function () {
-      resolve(xhr.response);
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error("Something went wrong!"));
+      }
+
       // const listsOfPosts = JSON.parse(xhr.response);
+    };
+
+    xhr.onerror = function () {
+      reject(new Error("Failed to send request!"));
     };
 
     xhr.send(JSON.stringify(data));
@@ -23,17 +32,21 @@ function sendHttpRequest(method, url, data) {
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    "GET",
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  const listsOfPosts = responseData;
-  for (const post of listsOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    postEl.querySelector("li").id = post.id;
-    listElement.append(postEl);
+  try {
+    const responseData = await sendHttpRequest(
+      "GET",
+      "https://jsonplaceholder.typicode.com/pos"
+    );
+    const listsOfPosts = responseData;
+    for (const post of listsOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (e) {
+    alert(e.message);
   }
 }
 
