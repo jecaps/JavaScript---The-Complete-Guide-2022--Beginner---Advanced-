81bc4dd385b53b9a4bc6f639efdbe01f9be7310f
Jerika Kanz <jerikacapila@gmail.com>
2022-03-03 10:31:57 +0100

    Building for Production

---
+++ section_21/webpack.config.prod.js
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint quotes: ["error", "double"] */
+/* eslint-env es6 */
+
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/app.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "assets", "scripts"),
+    publicPath: "assets/scripts",
+  },
+  devtool: "cheap-source-map",
+};
