1d35075329253f1054bc3e0c82c3ab2a9db567f3
Jerika Kanz <jerikacapila@gmail.com>
2022-03-07 14:18:01 +0100

    Working with Cookies

--- section_22/app.js
+++ section_22/app.js
@@ -3,9 +3,16 @@ const retrBtn = document.getElementById("retrieve-btn");
 
 storeBtn.addEventListener("click", () => {
   const userId = "u123";
-  document.cookie = `uid=${userId}`;
+  const user = { name: "Max", age: 30 };
+  document.cookie = `uid=${userId}; max-age=360`; // can also use expires that accepts date/time as value
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrBtn.addEventListener("click", () => {
   console.log(document.cookie);
+  const cookieData = document.cookie.split(";");
+  const data = cookieData.map((i) => {
+    return i.trim();
+  });
+  console.log(data[1].split("=")[1]); //returns user value '{ name: "Max", age: 30 }'
 });
