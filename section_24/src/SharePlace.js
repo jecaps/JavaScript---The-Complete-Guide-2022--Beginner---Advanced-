4837dde2bcba27c06ce9ea635996a1a20380f92e
Jerika Kanz <jerikacapila@gmail.com>
2022-03-08 09:55:08 +0100

    Getting the User Location

--- section_24/src/SharePlace.js
+++ section_24/src/SharePlace.js
@@ -6,7 +6,30 @@ class PlaceFinder {
     locateUserBtn.addEventListener("click", this.locateUserHandler);
     addressForm.addEventListener("submit", this.findAddressHandler);
   }
-  locateUserHandler() {}
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your browser - please use a more modern browser or manually enter an address."
+      );
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        alert(
+          "Could not locate you unfortunately. Please enter an address manually."
+        );
+      }
+    );
+  }
 
   findAddressHandler() {}
 }
+
+new PlaceFinder();
