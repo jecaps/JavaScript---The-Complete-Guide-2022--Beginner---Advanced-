cddd4235dc5e67908cf93a87ba14af3e95695588
Jerika Kanz <jerikacapila@gmail.com>
2022-03-08 19:51:28 +0100

    Understanding Symbols

---
+++ section_26/app.js
@@ -0,0 +1,20 @@
+// Library Land
+const uid = Symbol("uid");
+console.log(uid);
+
+const user = {
+  // id: "p1",
+  [uid]: "p1",
+  name: "Max",
+  age: 30,
+};
+
+user[uid] = "p3";
+
+// Appp Land => Using the Library
+
+user.id = "p2"; // this should not be possible
+
+console.log(user[Symbol("uid")]);
+console.log(Symbol("uid") === Symbol("uid"));
+console.log(user);
