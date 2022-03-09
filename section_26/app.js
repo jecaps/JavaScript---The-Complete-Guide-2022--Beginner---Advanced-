9650e11f71ad7b9ea7bd558ce7f2892f33843820
Jerika Kanz <jerikacapila@gmail.com>
2022-03-09 15:57:17 +0100

    the Proxy API and a 'First Trap'

--- section_26/app.js
+++ section_26/app.js
@@ -95,3 +95,17 @@ Reflect.setPrototypeOf(course, {
 });
 
 console.log(course.toString());
+
+const courseHandler = {
+  get(obj, propertyName) {
+    console.log(propertyName);
+    if (propertyName === "length") {
+      return 0;
+    }
+    return obj[propertyName] || "NOT FOUND";
+  },
+};
+
+const pCourse = new Proxy(course, courseHandler);
+console.log(pCourse.title, pCourse.length, pCourse.rating);
+console.log(course, pCourse);
