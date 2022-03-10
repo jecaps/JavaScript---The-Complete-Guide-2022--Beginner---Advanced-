5b8fa9b4913acce7a737f9658d8f27df5076c92f
Jerika Kanz <jerikacapila@gmail.com>
2022-03-10 19:24:35 +0100

    Adding the GET Location Route

--- section_27/routes/location.js
+++ section_27/routes/location.js
@@ -16,5 +16,14 @@ router.post("/add-location", (req, res, next) => {
   res.json({ message: "Stored location!", locId: id });
 });
 
-router.get("/add-location", (req, res, next) => {});
+router.get("/add-location/:lid", (req, res, next) => {
+  const locationId = +req.params.lid;
+  const location = locationStorage.locations.find((loc) => {
+    return loc.id === locationId;
+  });
+  if (!location) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  res.json({ address: location.address, coordinates: location.coords });
+});
 module.exports = router;
