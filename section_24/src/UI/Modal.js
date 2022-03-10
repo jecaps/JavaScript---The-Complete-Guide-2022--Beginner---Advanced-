aa503070a99c62a7fc07646feab0eb76157b4b45
Jerika Kanz <jerikacapila@gmail.com>
2022-03-10 16:43:01 +0100

    Understanding CORS (Cross Origin Resource Sharing)

--- section_24/src/UI/Modal.js
+++ section_24/src/UI/Modal.js
@@ -2,25 +2,26 @@ export class Modal {
   constructor(contentId, fallbackText) {
     this.fallbackText = fallbackText;
     this.contentTemplateEl = document.getElementById(contentId);
-    this.modalTemplateEl = document.getElementById("modal-template");
+    this.modalTemplateEl = document.getElementById('modal-template');
   }
 
   show() {
-    if ("content" in document.createElement("template")) {
+    if ('content' in document.createElement('template')) {
       const modalElements = document.importNode(
         this.modalTemplateEl.content,
         true
       );
-      this.modalElement = modalElements.querySelector(".modal");
-      this.backdropElement = modalElements.querySelector(".backdrop");
+      this.modalElement = modalElements.querySelector('.modal');
+      this.backdropElement = modalElements.querySelector('.backdrop');
       const contentElement = document.importNode(
         this.contentTemplateEl.content,
         true
       );
 
       this.modalElement.appendChild(contentElement);
-      document.body.insertAdjacentElement("afterbegin", this.modalElement);
-      document.body.insertAdjacentElement("afterbegin", this.backdropElement);
+
+      document.body.insertAdjacentElement('afterbegin', this.modalElement);
+      document.body.insertAdjacentElement('afterbegin', this.backdropElement);
     } else {
       // fallback code
       alert(this.fallbackText);
@@ -29,8 +30,7 @@ export class Modal {
 
   hide() {
     if (this.modalElement) {
-      document.body.removeChild(this.modalElement);
-      // this.modalElement.remove(); // for newer browsers
+      document.body.removeChild(this.modalElement); // this.modalElement.remove()
       document.body.removeChild(this.backdropElement);
       this.modalElement = null;
       this.backdropElement = null;
